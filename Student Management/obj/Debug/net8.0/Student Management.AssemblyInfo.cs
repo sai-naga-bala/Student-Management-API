fe3771543a5118bbd4a25de74f4955f7e7d416b5
Sai Naga Bala Kanchi <sainagabalakanchi717@gmail.com>
2025-05-18 00:57:28 +0530

    changes

--- Student Management/obj/Debug/net8.0/Student Management.AssemblyInfo.cs
+++ Student Management/obj/Debug/net8.0/Student Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb750ef99c9ef33f3819a5dd0919222b22345ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
